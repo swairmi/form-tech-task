283288be1084e97a68d3477c5f95072f73ae539c
JamesNorton <jimjamnorton@gmail.com>
2024-12-06 01:51:46 +0000

    exteneding the base home controller to allow the form submission with the data in hand

--- TechTaskCar/Controllers/HomeController.cs
+++ TechTaskCar/Controllers/HomeController.cs
@@ -7,10 +7,12 @@ namespace TechTaskCar.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+     private readonly TechTaskDbContext _context;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, TechTaskDbContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
     public IActionResult Index()
@@ -22,6 +24,27 @@ public class HomeController : Controller
     {
         return View();
     }
+
+            [HttpPost]
+        public IActionResult Index(Submission user)
+        {
+
+            Console.WriteLine("111"); 
+            if (ModelState.IsValid)
+            {
+
+                Console.WriteLine("222");
+                // Add the user to the database
+                _context.Submission.Add(user);
+                _context.SaveChanges();
+
+                // Redirect to another page or show success message
+                return RedirectToAction("Index");
+            }
+
+            // Return the form with validation errors
+            return View(user);
+        }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
